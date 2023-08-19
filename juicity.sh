9c6c9201a057af973b7f130686d188f5596ff1e2
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-08-20 02:03:09 +0330

    Update juicity.sh

--- juicity.sh
+++ juicity.sh
@@ -31,14 +31,14 @@ LATEST_RELEASE_URL=$(curl --silent "https://api.github.com/repos/juicity/juicity
 
 # Download and extract to /root/juicity
 mkdir -p /root/juicity
-curl -L $LATEST_RELEASE_URL -o /usr/local/bin/juicity.zip
-unzip -q /usr/local/bin/juicity.zip -d /root/juicity
+curl -L $LATEST_RELEASE_URL -o /root/juicity/juicity.zip
+unzip -q /root/juicity/juicity.zip -d /root/juicity
 
 # Delete all files except juicity-server
 find /root/juicity ! -name 'juicity-server' -type f -exec rm -f {} +
 
 # Set permissions
-chmod +x /usr/local/bin/juicity-server
+chmod +x /root/juicity/juicity-server
 
 # Read user input for configuration
 read -p "Enter listen port (or press enter to random port): " PORT
@@ -52,21 +52,21 @@ fi
 UUID=$(uuidgen)
 
 # Generate private key and certificate
-openssl ecparam -genkey -name prime256v1 -out /usr/local/bin/private.key
-openssl req -new -x509 -days 36500 -key /usr/local/bin/private.key -out /usr/local/bin/fullchain.cer -subj "/CN=speedtest.net"
+openssl ecparam -genkey -name prime256v1 -out /root/juicity/private.key
+openssl req -new -x509 -days 36500 -key /root/juicity/private.key -out /root/juicity/fullchain.cer -subj "/CN=speedtest.net"
 
 # Calculate the URL-safe Base64 encoded SHA-256 hash of the certificate chain
-CERT_HASH=$(openssl x509 -noout -fingerprint -sha256 -inform pem -in /usr/local/bin/fullchain.cer | awk -F '=' '{print $2}' | tr -d ':' | tr 'A-F' 'a-f' | xxd -r -p | base64 -w0 | sed 's/\//%2F/g' | sed 's/=/%3D/g')
+CERT_HASH=$(openssl x509 -noout -fingerprint -sha256 -inform pem -in /root/juicity/fullchain.cer | awk -F '=' '{print $2}' | tr -d ':' | tr 'A-F' 'a-f' | xxd -r -p | base64 -w0 | sed 's/\//%2F/g' | sed 's/=/%3D/g')
 
 # Create config_server.json
-cat > /usr/local/bin/config_server.json <<EOL
+cat > /root/juicity/config_server.json <<EOL
 {
   "listen": ":$PORT",
   "users": {
     "$UUID": "$PASSWORD"
   },
-  "certificate": "/usr/local/bin/fullchain.cer",
-  "private_key": "/usr/local/bin/private.key",
+  "certificate": "/root/juicity/fullchain.cer",
+  "private_key": "/root/juicity/private.key",
   "congestion_control": "bbr",
   "log_level": "info"
 }
@@ -83,9 +83,9 @@ After=network.target nss-lookup.target
 Type=simple
 User=root
 Environment=QUIC_GO_ENABLE_GSO=true
-ExecStart=/usr/local/bin/juicity-server run -c /usr/local/bin/config_server.json
-StandardOutput=file:/usr/local/bin/juicity-server.log
-StandardError=file:/usr/local/bin/juicity-server.log
+ExecStart=/root/juicity/./juicity-server run -c /root/juicity/config_server.json
+StandardOutput=file:/root/juicity/juicity-server.log
+StandardError=file:/root/juicity/juicity-server.log
 Restart=on-failure
 LimitNPROC=512
 LimitNOFILE=infinity
@@ -102,24 +102,15 @@ systemctl restart juicity
 
 
 # Prompt user for choice
-read -p "Select an option (1 or 2): 1) Hamrah-Aval--> IPV4   2) Irancell--> IPV6: " choice
+read -p "Select an option (1 or 2): 1) Irancell--> IPV6   2) Hamrah-Aval--> IPV4: " choice
 
 case $choice in
     1)
-        # Original share link
-        SHARE_LINK_IPV4=$("juicity://$UUID:$PASSWORD@[$IPv4_ADDRESS]:$PORT/?congestion_control=bbr&sni=www.speedtest.net&allow_insecure=0&pinned_certchain_sha256=$CERT_HASH")
-        echo "----------------------------------------------------------"
-        echo " "
-        echo "Link with IPv4: -->  $SHARE_LINK_IPV4"
-        echo "----------------------------------------------------------"
-        echo "$SHARE_LINK_IPV4" > link.txt  #save
-        ;;
-    2)
         # Get IPv6 address
         IPv6_ADDRESS=$(ip -6 addr show dev eth0 | awk '/inet6 .*global/{print $2}' | cut -d '/' -f 1)
-
+        
         # Original share link
-        SHARE_LINK_IPV4=$(/usr/local/bin/juicity-server generate-sharelink -c /usr/local/bin/config_server.json)
+        SHARE_LINK/root/juicity/juicity/juicity-server generate-share/root/juicity/juicity/config_server.json)
 
         # Replace IPv4 with IPv6 in the share link
         SHARE_LINK_IPV6=$(echo "$SHARE_LINK_IPV4" | sed "s/[0-9]\+\(\.[0-9]\+\)\{3\}/[$IPv6_ADDRESS]/g")
@@ -129,7 +120,16 @@ case $choice in
         echo "Link with IPv6: -->  $SHARE_LINK_IPV6"
         echo "----------------------------------------------------------"
         echo "$SHARE_LINK_IPV6" > link.txt  #save
-        ;;
+        ;;        
+    2)
+       # Original share link
+        SHARE_LINK_IPV4=$("juicity://$UUID:$PASSWORD@[$IPv4_ADDRESS]:$PORT/?congestion_control=bbr&sni=www.speedtest.net&allow_insecure=0&pinned_certchain_sha256=$CERT_HASH")
+        echo "----------------------------------------------------------"
+        echo " "
+        echo "Link with IPv4: -->  $SHARE_LINK_IPV4"
+        echo "----------------------------------------------------------"
+        echo "$SHARE_LINK_IPV4" > link.txt  #save
+        ;; 
     *)
         echo "Invalid choice. Please select 1 or 2."
         ;;
